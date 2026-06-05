cad0a83d824656e0b0bd7bc503c1f3c60302990d
Maria <mariazhukovskaya00@mail.ru>
2026-06-06 02:13:35 +0300

    changed

--- FurnitureFactory/settings.py
+++ FurnitureFactory/settings.py
@@ -28,10 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.environ.get("DEBUG") == "False":
-    DEBUG = False
-else:
-    DEBUG = True
+DEBUG = True
 
 SESSION_COOKIE_SECURE = True  
 CSRF_COOKIE_SECURE = True
