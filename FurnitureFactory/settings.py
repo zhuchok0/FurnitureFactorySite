4977b78b2fd8847fa6281168f1580e412b7d5b43
Maria <mariazhukovskaya00@mail.ru>
2026-06-07 20:07:46 +0300

    added docker and more flexible work with database

--- FurnitureFactory/settings.py
+++ FurnitureFactory/settings.py
@@ -84,12 +84,27 @@ WSGI_APPLICATION = 'FurnitureFactory.wsgi.application'
 import dj_database_url
 import os
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600
-    )
-}
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ['DATABASE_URL'],
+            conn_max_age=600
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.getenv('DATABASE_URL'),
+#         conn_max_age=600
+#     )
+# }
 
 # DATABASES = {
 #        'default': {
